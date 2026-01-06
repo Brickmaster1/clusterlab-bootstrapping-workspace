916deff154f68a0970470e93ce6d0051a97f8928
brickmasterhunt <brickmasterlogan@icloud.com>
2026-01-06 09:53:07 -0500

    Revise setup stuff

--- README.md
+++ README.md
@@ -2,16 +2,8 @@
 ## Reqiurements
 - #### A Linux machine of some form.
 - #### Docker (as well as Docker Compose), refer to the [official installation documentation](https://docs.docker.com/engine/install/).
-- #### Sysbox
-  you can follow installation instructions at the Sysbox [documentation](https://github.com/nestybox/sysbox/blob/master/docs/user-guide/install-package.md#installing-sysbox).
-
-  It's also available as an AUR package for Arch users as `sysbox-ce`
-
-  Once installed make sure that you start the service with
-  ```bash
-  sudo systemctl enable --now sysbox
-  ```
-
+- #### KVM/QEMU
+  
 ## Running
 #### First run the `setup.sh` script.  
   ```bash
--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 COMPOSE_FILE="compose.yaml"
+TARGET_LINE_GROUP_ADD="#group_add:"
+TARGET_LINE_VOLUME="- /var/run/docker.sock:/var/run/docker.sock"
+
 curl -O https://raw.githubusercontent.com/coder/coder/refs/heads/main/${COMPOSE_FILE}
 
-sed -i "/#group_add:/,/998\" # docker group on host/c\
-\    group_add:\n\      - \"$(getent group docker | cut -d: -f3)\" # docker group on host" ${COMPOSE_FILE}
+GROUP_ADD_INDENTATION=$(grep -oP "^\s*(?=${TARGET_LINE_GROUP_ADD})" ${COMPOSE_FILE} | head -1)
+DOCKER_GID=$(getent group docker | cut -d: -f3)
+
+REPLACEMENT_BLOCK="\
+${GROUP_ADD_INDENTATION}group_add:\n\
+${GROUP_ADD_INDENTATION}  - \"${DOCKER_GID}\" # docker group on host"
+
+sed -i "/${TARGET_LINE_GROUP_ADD}/,/998\" # docker group on host/c\\${REPLACEMENT_BLOCK}" ${COMPOSE_FILE}
+
+VOLUME_INDENTATION=$(grep -oP "^\s*(?=${TARGET_LINE_VOLUME})" ${COMPOSE_FILE} | head -1)
+NEW_VOLUMES="${VOLUME_INDENTATION}- /run/sysbox:/run/sysbox\\
+${VOLUME_INDENTATION}- /var/lib/sysbox:/var/lib/sysbox"
+sed -i "\#${TARGET_LINE_VOLUME}#a\\${NEW_VOLUMES}" ${COMPOSE_FILE}
